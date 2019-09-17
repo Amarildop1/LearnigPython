10c2b5d5793cb7c631dbd25512170ef6e279f0fd
Amarildo <amarildop1@live.com>
2019-09-16 22:45:14 -0300

    Calcula uma média ponderada entre duas notas.
    Usa pesos 6 e 4, respectivamente, para as duas notas informadas.

---
+++ MediaPonderada.py
@@ -0,0 +1,6 @@
+primeiraNota = int(input('Informe a primeira nota: '))
+segundaNota = int(input('Informe a segunda nota: '))
+
+mediaPonderada = (primeiraNota*6 + segundaNota*4)/10
+
+print(mediaPonderada)
