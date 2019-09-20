486adad79b626c5d61bd208ac483d6788c1799dd
Krueger <amarildop1dev@gmail.com>
2019-09-19 21:12:30 -0300

    atualizando

--- CalculaAreaDeTriangulo.py
+++ CalculaAreaDeTriangulo.py
@@ -1,4 +1,4 @@
-base = int(input('Informe o valor da base: '))
-altura = int(input('Informe o valor da altura: '))
-area = (base * altura)/2
-print('A area do triangulo eh: ', area)
+base = float(input('Informe o valor da base: '))
+altura = float(input('Informe o valor da altura: '))
+area = (base*altura)/2
+print('A area do triangulo eh:', area)
