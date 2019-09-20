ec025efb2b0553c92b2a0e06e32e43cbd24e7e8e
Krueger <amarildop1dev@gmail.com>
2019-09-19 22:21:31 -0300

    Troca o conteúdo entre duas variáveis

---
+++ TrocaConteudoDaVariavel.py
@@ -0,0 +1,11 @@
+num1 = int(input('Informe o primeiro numero: '))
+num2 = int(input('Informe o segundo numero: '))
+print('-------- Numeros Informados --------')
+print(num1)
+print(num2)
+aux = num1
+num1 = num2
+num2 = aux
+print('-------- Conteudos trocados --------')
+print(num1)
+print(num2)
