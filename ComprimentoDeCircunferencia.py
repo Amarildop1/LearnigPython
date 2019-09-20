701b75ff3bba09c376888a21c2f2a244a5159ce5
Krueger <amarildop1dev@gmail.com>
2019-09-19 21:18:34 -0300

    atualizando

---
+++ ComprimentoDeCircunferencia.py
@@ -0,0 +1,4 @@
+PI = 3.14
+raio = float(input('Informe o valor do Raio: '))
+comprimento = 2*PI*raio
+print(comprimento)
---
+++ NomeSobrenome.py
@@ -0,0 +1,5 @@
+primeiroNome = input('Informe seu nome: ')
+sobrenome = input('Informe seu sobrenome ')
+
+print(sobrenome,primeiroNome,sep=', ')
+print()
