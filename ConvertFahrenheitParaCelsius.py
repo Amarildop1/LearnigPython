a532ce992bcadc032774645b8ea49472e54f698e
Amarildo <amarildop1dev@gmail.com>
2019-09-20 11:23:26 -0300

    Converte temperatura de Fahrenheit em Grau Celsius

--- ConvertFahrenheitParaCelsius.py
+++ ConvertFahrenheitParaCelsius.py
@@ -1,3 +1,3 @@
-tempFahrenheit = int(input('Informe a temperatura em Fahrenheit: '))
+tempFahrenheit = float(input('Informe a temperatura em Fahrenheit: '))
 tempCelsius = (5/9)*(tempFahrenheit - 32)
 print('Grau Celsius: ',tempCelsius)
