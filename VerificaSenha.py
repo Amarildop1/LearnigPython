70b969957d629ab29949d03f3036b36b33a37705
Amarildo <amarildop1dev@gmail.com>
2019-10-14 23:41:46 -0300

    Verifica se a senha informada está correta.

---
+++ VerificaSenha.py
@@ -0,0 +1,22 @@
+#@Author Amarildo
+#L3-Q15 - 14.Out.2019
+'''
+Solicita uma senha do usuário e verifica se acertou.
+Senha correta: abcd
+Três tentativas erradas encerra o programa.
+'''
+senha = 'abcd'
+erro = 0 
+
+while(erro != 3):   #Com while True também funciona 
+    tentativa = input("\nInforme a Senha: ")
+    if(tentativa == senha):
+        print("Senha correta!")
+        break
+    else:
+        erro += 1
+        print("Senha incorreta.")
+        if(erro == 3):
+            break
+        print("Tente novamente!")
+        
