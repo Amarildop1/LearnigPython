6c6457d0074f9679a4d57a7a8d12af27248ece36
Amarildo <amarildop1dev@gmail.com>
2019-11-12 00:23:39 -0300

    Exibe os elementos da diagonal principal de uma matriz quadrada

--- ExibeDiagonalDeUmaMatrizQuadrada.py
+++ ExibeDiagonalDeUmaMatrizQuadrada.py
@@ -1,9 +1,3 @@
-"""
-    Dada uma matriz quadrada de elementos inteiros e ordem N,
-    exiba os elementos da diagonal principal.
-    Isto é, os elementos onde I = J. Obs: N será lido (N <= 10).
-"""
-
 matriz = []
 N = int(input("\nDigite a ordem 'N' da matriz: "))
 
@@ -27,3 +21,9 @@ for i in range(len(matriz)):
     for j in range(len(matriz)):
         if(matriz[i] == matriz[j]):
             print(matriz[i][j])
+
+"""
+    Dada uma matriz quadrada de elementos inteiros e ordem N,
+    exiba os elementos da diagonal principal.
+    Isto é, os elementos onde I = J. Obs: N será lido (N <= 10).
+"""
