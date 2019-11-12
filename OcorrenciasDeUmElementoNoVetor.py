898d66c8af0f79f4bb32551768efe7adbcee7a13
Amarildo <amarildop1dev@gmail.com>
2019-11-12 00:11:46 -0300

    Conta a quantidade de um determinado elemento dentro do vetor

--- OcorrenciasDeUmElementoNoVetor.py
+++ OcorrenciasDeUmElementoNoVetor.py
@@ -1,11 +1,5 @@
 import random
 
-"""
-    Leia um vetor V (contendo 30 elementos inteiros) 
-    e um valor inteiro K,
-    calcule e exiba a quantidade de ocorrências de K dentro de V.
-"""
-
 vetorV = []
 cont = 0
 
@@ -14,14 +8,14 @@ for i in range(30):
     vetorV.append( int( (random.random())*10 ) )
 
 """
-#Preenche o vetor com uma entrada
+#Preenche o vetor com entradas
 for i in range(10):
     vetorV.append(int(input("Digite um número: ")))
 """
 
 k = int(input("\nInforme o valor de 'K': "))
 
-#Contando a quantidade de ocorrências de K em vetorV
+#Contando a quantidade de ocorrências de K no vetorV
 for i in range(len(vetorV)):
     if (vetorV[i] == k):
         cont += 1
@@ -31,4 +25,11 @@ print("\nVetor A:", vetorV)
 if (cont == 0):
     print(f"\nNão há ocorrências do número '{k}' neste vetor!\n")
 else:
-    print(f"\nOcorrências do número '{k}' no vetor:",cont,)
+    print(f"\nOcorrências do número '{k}' no vetor:",cont)
+
+
+"""
+    Leia um vetor V (contendo 30 elementos inteiros) 
+    e um valor inteiro K,
+    calcule e exiba a quantidade de ocorrências de K dentro de V.
+"""
