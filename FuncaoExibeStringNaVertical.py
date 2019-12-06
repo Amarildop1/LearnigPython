18169068f05eaaa823dc0601ab7030183ce3878a
Amarildo <amarildop1dev@gmail.com>
2019-12-06 01:02:30 -0300

    Função que recebe uma string e a exibe na vertical

---
+++ FuncaoExibeStringNaVertical.py
@@ -0,0 +1,14 @@
+
+def exibeNaVertical(frase):
+    for i in range(len(frase)):
+        print(frase[i])
+
+entrada = input("Digite uma frase: ")
+
+exibeNaVertical(entrada)
+
+
+# List08 - Quest07
+# Funções
+# 06.Dez.2019
+# Função que recebe uma string e a exibe na vertical
