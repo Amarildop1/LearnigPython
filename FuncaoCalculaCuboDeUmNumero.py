837319ea7fbd9fcf7bc878cb2851d856e0b46dc6
Amarildo <amarildop1dev@gmail.com>
2019-12-05 22:56:24 -0300

    Função que recebe um número float e calcula o cubo.

--- FuncaoCalculaCuboDeUmNumero.py
+++ FuncaoCalculaCuboDeUmNumero.py
@@ -1,14 +1,16 @@
-#List08 - Quest01
-#Funções
-# 05.Dez.2019
-#Recebe um valor do tipo float e calcula o cubo.
 
 def cubo(num):
     return num**3
 
-    
+#Teste
 num = float(input("Digite um número: "))
 
 cuboDeNum = cubo(num)
 
-print(cuboDeNum)
+print(f"O cubo de {num} é:", cuboDeNum)
+
+
+#List08 - Quest01
+#Funções
+# 05.Dez.2019
+#Recebe um valor do tipo float e calcula o cubo.
