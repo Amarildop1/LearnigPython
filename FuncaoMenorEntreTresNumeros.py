435f98072379a2486cc34eea9cbdcda3d8d7afee
Amarildo <amarildop1dev@gmail.com>
2019-12-05 23:00:07 -0300

    Função que recebe três números e informa qual o menor.

--- FuncaoMenorEntreTresNumeros.py
+++ FuncaoMenorEntreTresNumeros.py
@@ -1,7 +1,3 @@
-#List08 - Quest02
-#Funções
-# 05.Dez.2019
-#Recebe 3 números e diz qual o menor
 
 def menorEntreOsTres(num1, num2, num3):
     if (num1 < num2 and num1 < num3):
@@ -15,10 +11,15 @@ def menorEntreOsTres(num1, num2, num3):
 
 #Programa teste
 num = []
-
 for i in range(3):
     num.append(float(input("Digite um número: ")))
 
 menor = menorEntreOsTres(num[0], num[1], num[2])
 
 print("O menor entre os três é:", menor)
+
+
+#List08 - Quest02
+#Funções
+# 05.Dez.2019
+#Recebe 3 números e diz qual o menor
